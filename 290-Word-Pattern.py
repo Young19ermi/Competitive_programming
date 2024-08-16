53f52e85394f98acd2641337e47fb90badd0fb2e
Ermias Seleshi Endale <111576904+Young19ermi@users.noreply.github.com>
2024-08-17 00:41:55 +0300

    [290] [Time Beats: 10.12%] [Memory Beats: 34.94%] - LeetPush

---
+++ 290-Word-Pattern.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        check = defaultdict(list)
+        l = list(pattern)
+        k = list(s.split())
+        if len(set(l)) != len(set(k)):
+            return False
+        if len(l) != len(k):
+            return False
+        for i in range(len(l)):
+            check[l[i]].append(k[i])
+        
+        for key,items in check.items():
+            if len(set(items)) > 1:
+                return False
+        return True 
