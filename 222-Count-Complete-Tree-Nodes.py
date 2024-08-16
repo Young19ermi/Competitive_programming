1551a977633463eebfcd517c0e0b334b707a0473
Ermias Seleshi Endale <111576904+Young19ermi@users.noreply.github.com>
2024-08-17 00:27:15 +0300

    [222] [Time Beats: 5.54%] [Memory Beats: 8.76%] - LeetPush

---
+++ 222-Count-Complete-Tree-Nodes.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        levels = defaultdict(list)
+        def solve(node,index):
+            if node:
+                solve(node.left,index+1)
+                solve(node.right,index+1)
+                levels[index].append(node.val)
+            res = 0
+            for key,items in levels.items():
+                res += len(items)
+            return res
+        return solve(root,0)
+
