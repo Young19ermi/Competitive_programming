f6235706337dbb1e5c3a97937d418bd7a036818e
Ermias Seleshi Endale <111576904+Young19ermi@users.noreply.github.com>
2024-09-03 00:54:11 +0300

    Update 2053-Kth-Distinct-String-in-an-Array.py

--- 2053-Kth-Distinct-String-in-an-Array.py
+++ 2053-Kth-Distinct-String-in-an-Array.py
@@ -1,6 +1,8 @@
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
         nums = []
+
+        #this is the solution
         for n in arr:
             if arr.count(n) == 1:
                 nums.append(n)
